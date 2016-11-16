830478e9d21802e945c736686cbcfaf46d33748a
sasabot <sasabuchi@jsk.imi.i.u-tokyo.ac.jp>
2016-11-16 15:53:58 +0900

    removing old examples

--- linux_kinect/KinectRgbdInteractionClient/examples/image.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rosservice call /kinect/request/image '{data: [{x: 0, y: 0, width: 1920, height: 1080}]}'
--- linux_kinect/KinectRgbdInteractionClient/examples/points.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rosservice call /kinect/request/points '{data: [{x: 0, y: 0, width: 512, height: 424}]}'
