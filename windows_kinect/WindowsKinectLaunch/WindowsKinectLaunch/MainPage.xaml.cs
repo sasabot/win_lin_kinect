98ee9a57ef251ebc26da725c8438f89b37b2bc1d
sasabot <sasabuchi@jsk.imi.i.u-tokyo.ac.jp>
2016-12-05 02:01:58 -0800

    auto terminate WindowsKinectLaunch

--- windows_kinect/WindowsKinectLaunch/WindowsKinectLaunch/MainPage.xaml.cs
+++ windows_kinect/WindowsKinectLaunch/WindowsKinectLaunch/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.System;
 using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace WindowsKinectLaunch
@@ -18,20 +19,26 @@ namespace WindowsKinectLaunch
             ApplicationView.PreferredLaunchViewSize = new Size(200, 100);
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
 
-            Task.Run(async () => {
+            var task1 = Task.Run(async () => {
                 var rgbdUri = new Uri("kinectrgbdinteraction:");
                 await Launcher.LaunchUriAsync(rgbdUri);
             });
 
-            Task.Run(async () => {
+            var task2 = Task.Run(async () => {
                 var rgbdUri = new Uri("kinectwindowsinteraction:");
                 await Launcher.LaunchUriAsync(rgbdUri);
             });
 
-            Task.Run(async () => {
+            var task3 = Task.Run(async () => {
                 var rgbdUri = new Uri("kinectmicrophoneinteraction:");
                 await Launcher.LaunchUriAsync(rgbdUri);
             });
+
+            task1.Wait();
+            task2.Wait();
+            task3.Wait();
+
+            Application.Current.Exit();
         }
     }
 }
