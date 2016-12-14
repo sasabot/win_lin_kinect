905723bbd1282143a0c5491e6328527d4b761da7
sasabot <sasabuchi@jsk.imi.i.u-tokyo.ac.jp>
2016-12-14 04:14:23 -0800

    adding image streaming to KinectRgbdInteraction, changing colorPoints to pre-process

--- windows_kinect/KinectRgbdInteraction/KinectRgbdInteraction/MainPage.xaml.cs
+++ windows_kinect/KinectRgbdInteraction/KinectRgbdInteraction/MainPage.xaml.cs
@@ -19,6 +19,7 @@ using Windows.Media.FaceAnalysis;
 using Windows.System;
 using System.Diagnostics;
 using Windows.UI.ViewManagement;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace KinectRgbdInteraction
 {
@@ -28,6 +29,13 @@ namespace KinectRgbdInteraction
         private MediaCapture mediaCapture = null;
         private Dictionary<MediaFrameSourceKind, MediaFrameReference> frames = null;
         private SemaphoreSlim frameProcessingSemaphore = new SemaphoreSlim(1);
+
+        private Point[] colorPoints = null;
+
+        private Dictionary<string, Func<byte[], bool>> requestHandlers = null;
+        private bool sendImageFlag = false;
+        private bool sendImageCenters = false;
+        private Point[] centersInPixel = null;
 
 #if PRINT_STATUS_MESSAGE
         private Windows.UI.Xaml.DispatcherTimer statusLogTimer = new Windows.UI.Xaml.DispatcherTimer();
@@ -44,6 +52,20 @@ namespace KinectRgbdInteraction
 
             if (localSettings.Values["mqttHostAddress"] != null)
                 this.IPText.Text = localSettings.Values["mqttHostAddress"].ToString();
+
+            this.colorPoints = new Point[640 * 360];
+            int row = 0;
+            int at = 0;
+            for (int idx = 0; idx < this.colorPoints.Length; ++idx) {
+                int y = at / 1920;
+                int x = at - y * 1920;
+                this.colorPoints[idx] = new Point(x, y);
+                at += 3;
+                if (at - row * 1920 >= 1920) {
+                    row += 3;
+                    at = row * 1920;
+                }
+            }
 
 #if PRINT_STATUS_MESSAGE
             this.statusLogTimer.Interval = TimeSpan.FromMilliseconds(100);
@@ -66,9 +88,18 @@ namespace KinectRgbdInteraction
 #endif
 
         private void Setup(string ip) {
+            if (this.requestHandlers == null) {
+                this.requestHandlers = new Dictionary<string, Func<byte[], bool>>() {
+                    { "/kinect/request/image", HandleRequestImage },
+                    { "/kinect/request/centers", HandleRequestImageCenters }
+                };
+            }
+
             if (this.client == null) {
                 this.client = new MqttClient(ip);
                 this.client.ProtocolVersion = MqttProtocolVersion.Version_3_1;
+                this.client.MqttMsgPublishReceived += this.onMqttReceive;
+                this.client.Subscribe(this.requestHandlers.Keys.ToArray(), Enumerable.Repeat(MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, this.requestHandlers.Count).ToArray());
                 this.client.Connect(Guid.NewGuid().ToString());
             }
 
@@ -153,28 +184,8 @@ namespace KinectRgbdInteraction
 
                     // map depth to color
                     // we will only create a reduced size map as original is too large : 1920 * 1080 -> 640 * 360
-                    int strideX = 3;
-                    int strideY = 3;
-                    int resizeWidth = 640;
-                    int resizeHeight = 360;
-
-                    Point[] colorPoints = new Point[resizeWidth * resizeHeight];
-                    int row = 0;
-                    int idx = 0;
-                    int at = 0;
-                    while (at < colorDesc.Width * colorDesc.Height) {
-                        int y = at / colorDesc.Width;
-                        int x = at - y * colorDesc.Width;
-                        colorPoints[idx] = new Point(x, y);
-                        ++idx;
-                        at += strideX;
-                        if (at - row * colorDesc.Width >= colorDesc.Width) {
-                            row += strideY;
-                            at = row * colorDesc.Width;
-                        }
-                    }
-                    Vector3[] points = new Vector3[colorPoints.Length];
-                    coordinateMapper.UnprojectPoints(colorPoints, this.frames[MediaFrameSourceKind.Color].CoordinateSystem, points);
+                    Vector3[] points = new Vector3[this.colorPoints.Length];
+                    coordinateMapper.UnprojectPoints(this.colorPoints, this.frames[MediaFrameSourceKind.Color].CoordinateSystem, points);
 
                     // stream point clouds
                     byte[] streamBytes = new byte[points.Length * 16];
@@ -184,7 +195,7 @@ namespace KinectRgbdInteraction
                             for (int i = range.Item1; i < range.Item2; ++i) {
                                 var values = new float[] { points[i].X, points[i].Y, points[i].Z };
                                 Buffer.BlockCopy(values, 0, streamBytes, j, 12); j += 12;
-                                Buffer.BlockCopy(colorBytes, Convert.ToInt32((colorPoints[i].Y * colorDesc.Width + colorPoints[i].X) * 4), streamBytes, j, 4); j += 4;
+                                Buffer.BlockCopy(colorBytes, Convert.ToInt32((this.colorPoints[i].Y * colorDesc.Width + this.colorPoints[i].X) * 4), streamBytes, j, 4); j += 4;
                             }
                         });
                     this.client.Publish("/kinect/stream/points", streamBytes);
@@ -193,6 +204,40 @@ namespace KinectRgbdInteraction
                     byte[] camIntr = new byte[16];
                     Buffer.BlockCopy(cameraInfo, 0, camIntr, 0, 16);
                     this.client.Publish("/kinect/stream/camerainfo", camIntr);
+
+                    // other requested queues (only one is processed at each frame)
+
+                    if (this.sendImageFlag) { // send full image if requested (full image should usually not be requested)
+                        byte[] bgrColorBytes = new byte[colorDesc.Width * colorDesc.Height * 3];
+                        Parallel.ForEach(System.Collections.Concurrent.Partitioner.Create(0, colorDesc.Height),
+                        (range) => {
+                            for (int i = range.Item1; i < range.Item2; ++i) {
+                                int srcIdx = i * colorDesc.Width * 4;
+                                int destIdx = i * colorDesc.Width * 3;
+                                for (int x = 0; x < colorDesc.Width; ++x)
+                                    Buffer.BlockCopy(colorBytes, srcIdx + x * 4, bgrColorBytes, destIdx + x * 3, 3);
+                            }
+                        });
+                        this.client.Publish("/kinect/stream/image", bgrColorBytes);
+                        this.sendImageFlag = false;
+                    } else if (this.sendImageCenters) { // send image centers if requested
+                        // get image centers from center pixels
+                        Vector3[] positionVectors = new Vector3[this.centersInPixel.Length];
+                        coordinateMapper.UnprojectPoints(this.centersInPixel, this.frames[MediaFrameSourceKind.Color].CoordinateSystem, positionVectors);
+
+                        // Vector3 -> float[]
+                        int numResults = positionVectors.Length;
+                        float[] positions = new float[positionVectors.Length * 3];
+                        for (int i = 0; i < positionVectors.Length; ++i)
+                            positionVectors[i].CopyTo(positions, i * 3);
+
+                        // float[] -> byte[]
+                        byte[] positionBytes = new byte[positions.Length * 4 + 2];
+                        Buffer.BlockCopy(BitConverter.GetBytes(numResults), 0, positionBytes, 0, 2);
+                        Buffer.BlockCopy(positions, 0, positionBytes, 2, positions.Length * 4);
+                        this.client.Publish("/kinect/stream/centers", positionBytes);
+                        this.sendImageCenters = false;
+                    }
 
 #if PRINT_STATUS_MESSAGE
                     ++this.kinectFrameCount;
@@ -210,6 +255,39 @@ namespace KinectRgbdInteraction
             } finally {
                 frameProcessingSemaphore.Release();
             }
+        }
+
+        private void onMqttReceive(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e) {
+            if (!this.requestHandlers.ContainsKey(e.Topic)) return;
+            this.requestHandlers[e.Topic](e.Message);
+        }
+
+        private bool HandleRequestImage(byte[] message) {
+            this.sendImageFlag = true;
+            return true;
+        }
+
+        private bool HandleRequestImageCenters(byte[] message) {
+            if (message.Length == 0) return false;
+
+            int numRequests = BitConverter.ToInt16(message, 0); // first 2 bytes is number of images
+            if (this.centersInPixel != null)
+                Array.Resize(ref this.centersInPixel, numRequests);
+            else
+                this.centersInPixel = new Point[numRequests];
+
+            // parse request (8 bytes per image)
+            int at = 2;
+            for (int i = 0; i < numRequests; ++i) {
+                int x = BitConverter.ToInt16(message, at);
+                int y = BitConverter.ToInt16(message, at + 2);
+                int width = BitConverter.ToInt16(message, at + 4);
+                int height = BitConverter.ToInt16(message, at + 6);
+                this.centersInPixel[i] = new Point(x + Convert.ToInt32(width * 0.5), y + Convert.ToInt32(height * 0.5));
+                at += 8;
+            }
+            this.sendImageCenters = true;
+            return true;
         }
 
         private void CloseApp_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
