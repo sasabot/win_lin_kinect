7156c2408091c3ac97ea413d98dd68fd492753be
sasabot <sasabuchi@jsk.imi.i.u-tokyo.ac.jp>
2016-12-14 04:19:55 -0800

    adding cascade classifier (openCV) windows runtime component for face detection

---
+++ windows_kinect/HaarRuntimeComponent/HaarRuntimeComponent/Class1.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "Class1.h"
+
+using namespace HaarRuntimeComponent;
+using namespace Platform;
+
+Class1::Class1(Platform::String^ _xml) : headDetector() {
+	auto fn = std::wstring(_xml->Data());
+	auto xml = std::string(fn.begin(), fn.end());
+
+	cv::FileStorage fs(xml, cv::FileStorage::READ | cv::FileStorage::MEMORY);
+	headDetector.read(fs.getFirstTopLevelNode());
+}
+
+Windows::Foundation::Collections::IVector<uint8>^ Class1::DetectMultiScale(const Platform::Array<uint8>^ image, int inwidth, int inheight) {
+	cv::Mat img(inheight, inwidth, CV_8U);
+	
+	int i = 0;
+	int j = 0;
+	for (int k = 0; k < image->Length; ++k) {
+		img.at<char>(i, j++) = image[k];
+		if (j >= img.cols) {
+			j = 0;
+			++i;
+		}
+	}
+
+	std::vector<cv::Rect> heads;
+	headDetector.detectMultiScale(img, heads);
+
+	auto res = ref new Platform::Collections::Vector<uint8>();
+	res->Append(heads.size());
+	for (auto it = heads.begin(); it != heads.end(); ++it) {
+		res->Append(static_cast<uint>(it->x));
+		res->Append(static_cast<uint>(it->y));
+		res->Append(static_cast<uint>(it->width));
+		res->Append(static_cast<uint>(it->height));
+	}
+
+	return res;
+}
---
+++ windows_kinect/HaarRuntimeComponent/HaarRuntimeComponent/Class1.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <opencv2/core/utility.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+
+namespace HaarRuntimeComponent
+{
+    public ref class Class1 sealed
+    {
+    public:
+        Class1(Platform::String^ _xml);
+
+		Windows::Foundation::Collections::IVector<uint8>^ DetectMultiScale(const Platform::Array<uint8>^ image, int inwidth, int inheight);
+
+	private:
+		cv::CascadeClassifier headDetector;
+    };
+}
