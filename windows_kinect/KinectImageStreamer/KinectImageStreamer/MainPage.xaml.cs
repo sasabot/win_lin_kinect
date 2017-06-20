e557ee9f53facd19dee98d342b7e1a6d54b04138
sasabot <sasabuchi@jsk.imi.i.u-tokyo.ac.jp>
2017-06-20 10:05:08 -0700

    adding KinectImageStreamer app

---
+++ windows_kinect/KinectImageStreamer/KinectImageStreamer/MainPage.xaml.cs
@@ -0,0 +1,194 @@
+﻿#define PRINT_STATUS_MESSAGE
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using uPLibrary.Networking.M2Mqtt;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+using System.Threading.Tasks;
+using Windows.Media.Capture;
+using Windows.Media.Capture.Frames;
+using Windows.Media.Devices.Core;
+using Windows.Media;
+using System.Threading;
+using Windows.Graphics.Imaging;
+using System.Numerics;
+using Windows.Media.FaceAnalysis;
+using Windows.System;
+using System.Diagnostics;
+using Windows.UI.ViewManagement;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using Windows.System.Display;
+
+namespace KinectImageStreamer
+{
+    public sealed partial class MainPage : Page
+    {
+        private MqttClient client = null;
+        private MediaCapture mediaCapture = null;
+        private Dictionary<MediaFrameSourceKind, MediaFrameReference> frames = null;
+        private SemaphoreSlim frameProcessingSemaphore = new SemaphoreSlim(1);
+
+        private Point[] colorPoints = null;
+
+        private readonly DisplayRequest displayRequest = new DisplayRequest();
+
+#if PRINT_STATUS_MESSAGE
+        private Windows.UI.Xaml.DispatcherTimer statusLogTimer = new Windows.UI.Xaml.DispatcherTimer();
+        private Stopwatch appClock = new Stopwatch();
+        private uint kinectFrameCount = 0;
+#endif
+
+        private Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+        public MainPage() {
+            this.InitializeComponent();
+            ApplicationView.PreferredLaunchViewSize = new Size(350, 350);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
+            if (localSettings.Values["mqttHostAddress"] != null)
+                this.IPText.Text = localSettings.Values["mqttHostAddress"].ToString();
+
+#if PRINT_STATUS_MESSAGE
+            this.statusLogTimer.Interval = TimeSpan.FromMilliseconds(100);
+            this.statusLogTimer.Tick += StatusLogTick;
+            this.statusLogTimer.Start();
+#endif
+
+            displayRequest.RequestActive();
+        }
+
+        private void StartApp_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
+            localSettings.Values["mqttHostAddress"] = this.IPText.Text;
+            this.Setup(this.IPText.Text);
+        }
+
+#if PRINT_STATUS_MESSAGE
+        private void StatusLogTick(object sender, object e) {
+            this.MemoryMonitor.Text = "MemoryUsage: " + Convert.ToString(MemoryManager.AppMemoryUsage / 1048576);
+            if (this.appClock.IsRunning)
+                this.KinectFPS.Text = "KinectFPS: " + Convert.ToString(Convert.ToInt32(kinectFrameCount / this.appClock.Elapsed.TotalSeconds));
+        }
+#endif
+
+        private void Setup(string ip) {
+            if (this.client == null) {
+                this.client = new MqttClient(ip);
+                this.client.ProtocolVersion = MqttProtocolVersion.Version_3_1;
+                this.client.Connect(Guid.NewGuid().ToString());
+            }
+
+            if (this.frames == null)
+                this.frames = new Dictionary<MediaFrameSourceKind, MediaFrameReference>() {
+                    { MediaFrameSourceKind.Color, null }
+                };
+
+            if (this.mediaCapture == null) {
+                // select device with both color and depth streams
+                var cameras = Task.Run(async () => { return await MediaFrameSourceGroup.FindAllAsync(); });
+                var eligible = cameras.Result.Select(c => new {
+                    Group = c,
+                    SourceInfos = new MediaFrameSourceInfo[] {
+                    c.SourceInfos.FirstOrDefault(info => info.SourceKind == MediaFrameSourceKind.Color),
+                    c.SourceInfos.FirstOrDefault(info => info.SourceKind == MediaFrameSourceKind.Depth)
+                    }
+                }).Where(c => c.SourceInfos[0] != null && c.SourceInfos[1] != null).ToList();
+                if (eligible.Count == 0) return;
+                var selected = eligible[0];
+
+                // open device
+                this.mediaCapture = new MediaCapture();
+                Task.Run(async () => {
+                    await this.mediaCapture.InitializeAsync(new MediaCaptureInitializationSettings {
+                        SourceGroup = selected.Group,
+                        SharingMode = MediaCaptureSharingMode.SharedReadOnly,
+                        StreamingCaptureMode = StreamingCaptureMode.Video,
+                        MemoryPreference = MediaCaptureMemoryPreference.Cpu
+                    });
+                }).Wait();
+
+                // set stream callbacks
+                for (int i = 0; i < selected.SourceInfos.Length; ++i) {
+                    MediaFrameSourceInfo info = selected.SourceInfos[i];
+                    MediaFrameSource frameSource = null;
+                    if (this.mediaCapture.FrameSources.TryGetValue(info.Id, out frameSource)) {
+                        var frameReader = Task.Run(async () => { return await this.mediaCapture.CreateFrameReaderAsync(frameSource); });
+                        frameReader.Result.FrameArrived += FrameReader_FrameArrived;
+                        var status = Task.Run(async () => { return await frameReader.Result.StartAsync(); });
+                        if (status.Result != MediaFrameReaderStartStatus.Success) return;
+                    }
+                }
+            }
+
+#if PRINT_STATUS_MESSAGE
+            this.appClock.Start();
+#endif
+        }
+
+        private void FrameReader_FrameArrived(MediaFrameReader sender, MediaFrameArrivedEventArgs args) {
+            if (!frameProcessingSemaphore.Wait(0)) return;
+
+            try {
+                var frame = sender.TryAcquireLatestFrame();
+                if (frame != null) this.frames[frame.SourceKind] = frame;
+
+                if (this.frames[MediaFrameSourceKind.Color] != null) {
+                    var colorDesc = this.frames[MediaFrameSourceKind.Color].VideoMediaFrame.SoftwareBitmap.LockBuffer(BitmapBufferAccessMode.Read).GetPlaneDescription(0);
+
+                    // get color information
+                    var bitmap = SoftwareBitmap.Convert(this.frames[MediaFrameSourceKind.Color].VideoMediaFrame.SoftwareBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Ignore);
+                    byte[] colorBytes = new byte[bitmap.PixelWidth * bitmap.PixelHeight * 4];
+                    bitmap.CopyToBuffer(colorBytes.AsBuffer());
+
+                    byte[] bgrColorBytes = new byte[colorDesc.Width * colorDesc.Height * 3];
+                    Parallel.ForEach(System.Collections.Concurrent.Partitioner.Create(0, colorDesc.Height),
+                    (range) => {
+                        for (int i = range.Item1; i < range.Item2; ++i) {
+                            int srcIdx = i * colorDesc.Width * 4;
+                            int destIdx = i * colorDesc.Width * 3;
+                            for (int x = 0; x < colorDesc.Width; ++x)
+                                Buffer.BlockCopy(colorBytes, srcIdx + x * 4, bgrColorBytes, destIdx + x * 3, 3);
+                        }
+                    });
+                    this.client.Publish("/kinect/stream/image/hd", bgrColorBytes);
+
+#if PRINT_STATUS_MESSAGE
+                    ++this.kinectFrameCount;
+#endif
+
+                    bitmap.Dispose();
+                    this.frames[MediaFrameSourceKind.Color].Dispose();
+                    this.frames[MediaFrameSourceKind.Color] = null;
+                }
+            } catch (Exception ex) {
+                // TODO
+            } finally {
+                frameProcessingSemaphore.Release();
+            }
+        }
+
+        private void CloseApp_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
+            while (!frameProcessingSemaphore.Wait(0)) continue;
+
+            try {
+                if (this.mediaCapture != null) {
+                    this.mediaCapture.Dispose();
+                    this.mediaCapture = null;
+                }
+
+                if (this.client != null) {
+                    this.client.Disconnect();
+                    this.client = null;
+                }
+            } finally {
+                frameProcessingSemaphore.Release();
+            }
+
+#if PRINT_STATUS_MESSAGE
+            this.appClock.Stop();
+#endif
+        }
+    }
+}
