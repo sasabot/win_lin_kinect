dc88868517f7957c775fd32f9faa2c691995993f
sasabot <sasabuchi@jsk.imi.i.u-tokyo.ac.jp>
2017-11-15 23:31:23 +0900

    remove opencv files not found from WindowsInterface.hh

--- linux_kinect/include/linux_kinect/WindowsInterface.hh
+++ linux_kinect/include/linux_kinect/WindowsInterface.hh
@@ -3,8 +3,8 @@
 
 #include <ros/ros.h>
 
-#include <opencv2/core/utility.hpp>
-#include <opencv2/highgui.hpp>
+// #include <opencv2/core/utility.hpp>
+// #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
