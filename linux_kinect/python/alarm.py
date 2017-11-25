a8de53a3db7c3380451b64f678be69871ce5b870
sasabot <sasabuchi@jsk.imi.i.u-tokyo.ac.jp>
2017-11-25 21:39:56 +0900

    adding alarm handler linux

---
+++ linux_kinect/python/alarm.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# brief: rings an alarm on windows linked by /topic
+# usage: warn with an alarm when /topic is called
+
+import paho.mqtt.client as mqtt
+import rospy
+from std_msgs.msg import *
+
+port = 1883
+
+def on_connect(client, userdata, flags, respons_code):
+    print('status {0}'.format(respons_code))
+
+def on_rosmessage(msg):
+    client.publish('/windows/alarm', alarm)
+
+if __name__ == '__main__':
+    rospy.init_node('windows_alarm_node')
+    sub = rospy.Subscriber('/topic', Empty, on_rosmessage)
+
+    host = rospy.get_param('~ip')
+    # ~alarm:
+    # Default, IM, Mail, Reminder, SMS,
+    # Alarm, Alarm.2 ~ Alarm.10, Call, Call.2 ~ Call.10
+    alarm = rospy.get_param('~alarm')
+
+    if 'Alarm' in alarm or 'Call' in alarm:
+        alarm = 'ms-winsoundevent:Notification.Looping.' + alarm
+    else:
+        alarm = 'ms-winsoundevent:Notification.' + alarm
+
+    client = mqtt.Client(protocol=mqtt.MQTTv31)
+    client.on_connect = on_connect
+    client.connect(host, port=port, keepalive=60)
+    client.loop_start()
+
+    r = rospy.Rate(60)
+    while not rospy.is_shutdown():
+        r.sleep()
